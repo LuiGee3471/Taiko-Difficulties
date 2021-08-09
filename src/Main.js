77780dd62060ea0649d08ed895e51fad9bdef246
LuiGee3471 <yjs3475@gmail.com>
2021-08-09 22:27:19 +0900

    기본 컴포넌트 추가

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SongList from './SongList';
+
+export default function Main() {
+    return (
+        <main className="bg-body mt-12">
+            <SongList />
+        </main>
+    )
+}
---
+++ src/SongList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SongList() {
+    return (
+        <main className="bg-body mt-12">
+
+        </main>
+    )
+}
