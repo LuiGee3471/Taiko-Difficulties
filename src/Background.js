640e0a9ee1b7f318098d827d69d463965a27c797
LuiGee3471 <yjs3475@gmail.com>
2021-08-20 00:24:40 +0900

    배경 최소 높이 지정

--- src/Background.js
+++ src/Background.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Background() {
     return (
-        <div className="bg-body opacity-25 absolute w-full h-full z-0"></div>
+        <div className="bg-body opacity-25 absolute w-full h-full z-0 min-h-screen"></div>
     )
 }
